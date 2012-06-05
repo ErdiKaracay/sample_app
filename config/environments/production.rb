5b4e714a47f761e23158dfd432563fba1cd4fac4
Erdi Karacay <erdi-karacay@hotmail.com>
2012-06-05 22:46:01 +0300

    Changed config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,12 +15,11 @@ SampleApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.precompile += %w( blueprint/screen.css blueprint/print.css )
 
 
   # Defaults to Rails.root.join("public/assets")
