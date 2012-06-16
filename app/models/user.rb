2b31322b403618b72e104f859f8bec2d5638909e
Erdi Karacay <erdi-karacay@hotmail.com>
2012-06-17 00:51:31 +0300

    Updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessor   :password
   attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
 
@@ -14,38 +14,5 @@ class User < ActiveRecord::Base
   validates :password, :presence     => true,
                        :confirmation => true,
                        :length       => { :within => 6..40 }
+  validates :password_confirmation, :presence => true
 
-  before_save :encrypt_password
-
-  def has_password?(submitted_password)
-    encrypted_password ==encrypt(submitted_password)
-  end
-
-  class << self
-    def authenticate(email, submitted_password)
-      user = find_by_email(email)
-      return nil  if user.nil?
-      return user if user.has_password?(submitted_password)
-    end
-  end
-
-
-  private
-
-    def encrypt_password
-      self.salt = make_salt if new_record?
-      self.encrypt_password = encrypt(password)
-    end
-
-    def encrypt(string)
-      secure_hash("#{salt}--#{string}")
-    end
-
-    def make_salt
-      secure_hash("#{Time.now.utc}--#{password}")
-    end
-
-    def secure_hash(string)
-      Digest::SHA2.hexdigest(string)
-    end
-end
